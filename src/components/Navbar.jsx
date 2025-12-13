3b1cdbe48152ecc52505a280cc3bb7dbf3825980
Petertechy <petertechy01@gmail.com>
2025-12-13 23:52:14 +0100

    Update Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,20 @@ const Navbar = () => {
   return (
     <>
       <div className="row">
-        <div className="col-5">
+        <div className="col-3">
           <h1 className={style.text}>Nav Bar</h1>
         </div>
 
         <div className="col-7">
           <ul className="d-flex gap-5 text-success fw-bold">
           <li><Link className="text-success" to="/">Home</Link></li>
-          <li><Link className="text-success" to="/product-page">Product Redux</Link></li>
+          <li><Link className="text-success" to="/register">Register</Link></li>
+          <li><Link className="text-success" to="/signin">Sign In</Link></li>
+          <li><Link className="text-success" to="/user-dashboard">Dashboard</Link></li>
+          {/* <li><Link className="text-success" to="/product-page">Product Redux</Link></li>
           <li><Link className="text-success" to="/contact">Contact</Link></li>
           <li><Link className="text-success" to="/about">About</Link></li>
-          <li><Link className="text-success" to="/signup">Register</Link></li>
-          <li><Link className="text-success" to="/effect">Effect</Link></li>
+          <li><Link className="text-success" to="/effect">Effect</Link></li> */}
         </ul>
         </div>
       </div>
